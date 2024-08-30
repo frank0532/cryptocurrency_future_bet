968005a155d6fa9dfce96e102fc271c5054a20b8
frank cao <caofa@foxmail.com>
2024-08-30 17:10:29 +0800

    testnet switches to 'Sepolia' from 'Georli'.

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,8 +7,8 @@ require("hardhat-contract-sizer")
 require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
-const GOERLI_RPC_URL =
-    process.env.GOERLI_RPC_URL
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
@@ -21,10 +21,10 @@ module.exports = {
         localhost: {
             chainId: 31337,
         },
-        goerli: {
-            url: GOERLI_RPC_URL,
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
             accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            chainId: 5,
+            chainId: 11155111,
         },
     },
     solidity: "0.8.17",
